d500093414f794ff4b72b483bc2d133a3acf609a
Sudipta Karmakar <85438511+sudipta7431@users.noreply.github.com>
2022-09-16 02:15:52 +0530

    Add files via upload

--- game.py
+++ game.py
@@ -4,26 +4,24 @@ import random
 #GAME CLASS FOR RUN THE GAME LOGIC
 class Game:
     counter = 1
+    ran = random.randint(0, 10)
     def Player(self, name):
         self.name = name
         print("Player: %s" % self.name)
 
     #LOGIC OF THE GAME
-    def startGame(self, y, x):
-        if y != x:
+    def startGame(self, y):
+        if y != Game.ran:
             Game.counter += 1
             main2()
             self.startGame(y)
         else:
             print("GOOD JOB YOUR ARE GASS RIGHT NO \n YOUR CHOSE! ATTAMPT NO IS : ", Game.counter)
-            print("The random number is : " + str(x))
+            print("The random number is : " + str(Game.ran))
             exit()
 
 
-#GAME RANDOM NUMBER GENARETOR
-def random_no():
-    x = random.randint(0, 10)
-    return x
+
 
 #MAIN FUNCTION FOR PLAYER NAME INPUT
 def main1():
@@ -31,15 +29,14 @@ def main1():
     n1 = input("Enter the name of the player : ")
     c.Player(n1)
 
-#2ND MAIN FUNCTION FOR PLAYER IMPUT IN THE PLAYING TIME
+#MAIN FUNCTION FOR START THE GAME
 def main2():
     c = Game()
-    ran = random_no()
     n2 = int(input("Enter your gauss number(0 to 10): "))
-    c.startGame(n2,ran)
+    c.startGame(n2)
 
 
 if __name__ == "__main__":
     main1()
-    random_no()
     main2()
+
